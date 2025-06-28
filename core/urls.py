dc78aa58e0ad1340a65bc23b3fd8bd2249f84633
Jal7823 <Jal7823@gmail.com>
2025-06-28 16:35:03 +0200

    feat: :arrow_up: Update core
    Update core,separate develoment,test and productions files and update home page of api

--- core/urls.py
+++ core/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     #Docs
     path('api/', include('apps.products.api.router')),  # Products API
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # Auth
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+from dotenv import load_dotenv
 
-from django.core.wsgi import get_wsgi_application
+# Cargar el .env desde la raíz del proyecto o donde esté ubicado
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
